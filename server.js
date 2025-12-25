64226492d1043933995b18cf345e86cb2467152d
Radhika <mukeshmadaan2025@gmail.com>
2025-12-25 17:01:04 +0530

    Added the deployed link for the Project

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5173", "http://127.0.0.1:5501"],
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5173", "http://127.0.0.1:5501","https://crowdfundingfrontend.onrender.com"],
     credentials: true,
     exposedHeaders: ["Content-Type", "Authorization"],
   })
